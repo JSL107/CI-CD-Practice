5484491b3c2f441fda61baad7e01c01a97e9915f
LJS107 <juneseok0107@naver.com>
2025-06-10 13:54:14 +0900

    fix(release.yml): 실제 release 됐을 때만 알람 가게 설정

--- src/main/java/com/example/auth/healthCheck/HealCheckController.java
+++ src/main/java/com/example/auth/healthCheck/HealCheckController.java
@@ -1,6 +1,7 @@
 package com.example.auth.healthCheck;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HealCheckController {
     @GetMapping("/health")
     public String health() {
         return "App is running";
+    }
+
+    @PostMapping("/check")
+    public String check() {
+        return "Finished Checking";
     }
 }
