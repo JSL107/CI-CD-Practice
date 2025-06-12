336aeee808348562119275c6a5db70095869e817
LJS107 <juneseok0107@naver.com>
2025-06-12 13:49:09 +0900

    fix : 쓸모없는 설정값 제거

--- src/main/java/com/example/auth/healthCheck/HealCheckController.java
+++ src/main/java/com/example/auth/healthCheck/HealCheckController.java
@@ -1,7 +1,6 @@
 package com.example.auth.healthCheck;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,10 +9,5 @@ public class HealCheckController {
     @GetMapping("/health")
     public String health() {
         return "App is running";
-    }
-
-    @PostMapping("/check")
-    public String check() {
-        return "Finished Checking";
     }
 }
